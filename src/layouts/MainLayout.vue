7a52af62f112f6006c2919a483ea351bd3679b99
Mehmet Uysal <mehmetuysal2000@yandex.com>
2023-01-04 13:46:18 +0300

    1.0.1

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,6 +12,20 @@ export default defineComponent({
     return {
       img: require("@/assets/logo.jpg")
     }
+  },
+  mounted() {
+    const text = `
+                                   .oooo.     .oooo.     .oooo.     .oooo.   
+                                 .dP""Y88b   d8P' Y8b   d8P' Y8b   d8P' Y8b  
+oooo  oooo  oooo    ooo  .oooo.o       ]8P' 888    888 888    888 888    888 
+ 888   888    88.  .8'  d88(  "8     .d8P'  888    888 888    888 888    888 
+ 888   888     88..8'    "Y88b.    .dP'     888    888 888    888 888    888 
+ 888   888      888'    o.  )88b .oP     .o '88b  d88' '88b  d88'  88b  d88' 
+ 'V88V"V8P'     .8'     8""888P' 8888888888  'Y8bd8P'   'Y8bd8P'   'Y8bd8P'  
+            .o..P'                                                           
+            'Y8P'
+`
+    console.log(text)
   }
 })
 </script>
